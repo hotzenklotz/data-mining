b1bcfa65a66abf6266b9c67be92580fdaf7e3bac
Georg Wiese <georgwiese@gmail.com>
2015-05-16 16:54:15 +0200

    Implement growTree

--- Assignment 3/assignment3.R
+++ Assignment 3/assignment3.R
@@ -1,2 +1,38 @@
 
-data = read.csv("data.csv")
+allData = read.csv("data.csv")
+
+# Dummy implementations for now
+
+homogeneous = function(data) {
+  return(F)
+}
+
+label = function(data) {
+  return("Gifts")
+}
+
+bestSplit = function(data) {
+  return("Textiles")
+}
+
+growTree = function(data) {
+  if (homogeneous(data)) {
+    return(list(label=label(data)))
+  }
+  
+  splitFeature = bestSplit(data)
+  literals = unique(allData[splitFeature])
+  children = NULL
+  
+  for (literal in literals) {
+    dataSubset = subset(allData, allData[splitFeature] == "medium")
+    
+    if (nrow(dataSubset) > 0) {
+      children = c(children, growTree(dataSubset))
+    } else {
+      children = c(children, list(label=label(dataSubset)))
+    }
+  }
+  
+  return(list(label=splitFeature, children=children))
+}

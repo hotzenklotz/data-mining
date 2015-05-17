376cd96e9534a2b105502067b2c79865be4c06b9
Georg Wiese <georgwiese@gmail.com>
2015-05-17 21:37:35 +0200

    Clarify and document data structures

--- Assignment 3/assignment3.R
+++ Assignment 3/assignment3.R
@@ -15,9 +15,26 @@ bestSplit = function(data) {
   return("Textiles")
 }
 
-growTree = function(data) {
+
+# Represent the decision tree by its root node.
+# 
+# Nodes have the following attributes:
+# - splitFeature (only internal nodes) Feature by which children are distinguished
+# - children (only internal nodes) Vector of children nodes
+# - label (only leaf nodes) Predicted class
+# - edgeValue Value of splitFeature as defined by the parent node
+
+leafNode = function(label, edgeValue) {
+  return(list(label=label, edgeValue=edgeValue))
+}
+
+internalNode = function(splitFeature, children, edgeValue) {
+  return(list(splitFeature=splitFeature, children=children, edgeValue=edgeValue))
+}
+
+growTree = function(data, edgeValue=NULL) {
   if (homogeneous(data)) {
-    return(list(label=label(data)))
+    return(leafNode(label(data), edgeValue))
   }
   
   splitFeature = bestSplit(data)
@@ -28,11 +45,11 @@ growTree = function(data) {
     dataSubset = subset(allData, allData[splitFeature] == literal)
     
     if (nrow(dataSubset) > 0) {
-      children = c(children, growTree(dataSubset))
+      children = c(children, growTree(dataSubset, literal))
     } else {
-      children = c(children, list(label=label(dataSubset)))
+      children = c(children, leafNode(label(dataSubset), literal))
     }
   }
   
-  return(list(label=splitFeature, children=children))
+  return(internalNode(splitFeature, children, edgeValue))
 }

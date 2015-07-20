3561c2db09815e7d98c80e52415ed7cb58fbf9ed
Tom Herold <tom.herold@scalableminds.com>
2015-07-20 14:19:01 +0200

    added LDA for comparison

--- Assignment 7/assignment7.R
+++ Assignment 7/assignment7.R
@@ -1,5 +1,6 @@
 # 3 Ada Boosting
 library("ada")
+library(MASS)
 
 ## Override iris species with TRUE / FALSE for binary classifier for "setosa"
 iris[, 5] <- as.logical(iris[, 5] == "Iris-setosa")
@@ -14,6 +15,7 @@ train_ind <- sample(seq_len(n), size = sample_size)
 
 train <- iris[train_ind, ]
 test <- iris[-train_ind, ]
+validation <- iris[-train_ind, 1:4]
 
 classifier <- ada(Species~., data = train, type = "gentle", iter = 70)
 classifier <- addtest(classifier, test[, -nrow(test)], test[, nrow(test)])
@@ -24,12 +26,31 @@ pairs(classifier, train ,maxvar=2)
 summary(classifier)
 
 ## Prediciton + Evalution
-prediction <- predict(classifier, test[, 1:4])
 
-truePositives <- test[, 5] & as.logical(prediction)
-truePositives <- truePositives[truePositives == TRUE]
+evaluate <- function(prediciton, test) {
+  
+  truePositives <- test[, 5] & as.logical(prediction)
+  truePositives <- truePositives[truePositives == TRUE]
+  
+  trueNegatives <-  !test[, 5] & !as.logical(prediction)
+  trueNegatives <- trueNegatives[trueNegatives == TRUE]
+  
+  accuracy <- (length(truePositives) + length(trueNegatives)) / nrow(test)
+  
+  list(accuracy = accuracy, TN = trueNegatives, TP = truePositives)
+}
 
-trueNegatives <-  !test[, 5] & !as.logical(prediction)
-trueNegatives <- trueNegatives[trueNegatives == FALSE]
+predictions <- predict(classifier, validation)
+print(evaluate(predictions, test))
 
-accuracy <- (length(truePositives) + length(trueNegatives)) / nrow(test)
+## 4 Comparison
+
+# Linear Classifier
+fit <- vglm(Species~., family=multinomial, data=iris)
+predictions <- predict(fit, validation)$class
+print(evaluate(predictions, test))
+
+#LDA
+fit <- lda(Species~., data=train)
+predictions <- predict(fit, validation)$class
+print(evaluate(predictions, test))
